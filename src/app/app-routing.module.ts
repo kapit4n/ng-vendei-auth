8404cba71cd1251c1ffeb27149e758ecc97631e1
Luis Arce <luis.arce22@gmail.com>
2019-12-28 22:25:53 -0400

    init login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/pages/auth/login/login.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: LoginComponent,
+  data: { title: "Login" }
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
